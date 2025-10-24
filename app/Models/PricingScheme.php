6126f2310f74d6b9eca3f4324c65ac3a6fdedd4c
Amru Dzaky <amrudzr@gmail.com>
2025-10-24 07:23:16 +0700

    fix(style): apply code style fixes (Laravel Pint)

--- app/Models/PricingScheme.php
+++ app/Models/PricingScheme.php
@@ -2,16 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PricingScheme extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
-    
+
     protected function casts(): array
     {
         return [
