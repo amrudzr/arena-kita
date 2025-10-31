a7926f6e97c639044f5cecfbee83c240d695ace2
Amru Dzaky <amrudzr@gmail.com>
2025-10-31 11:44:41 +0700

    feat(profile): create UpdateProfileRequest for validation

---
+++ app/Http/Requests/API/Profile/UpdateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\API\Profile;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $userId = Auth::guard('api_user')->id();
+
+        return [
+            'full_name' => 'required|string|max:100',
+            'phone_number' => 'nullable|string|max:20',
+            // 'profile_photo' => 'nullable|image|mimes:jpg,jpeg,png|max:2048', 
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'full_name.required' => 'Nama lengkap wajib diisi.',
+            'full_name.max' => 'Nama lengkap tidak boleh lebih dari 100 karakter.',
+            'phone_number.max' => 'Nomor telepon tidak boleh lebih dari 20 karakter.',
+        ];
+    }
+}
