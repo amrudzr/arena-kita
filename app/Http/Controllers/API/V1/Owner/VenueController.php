52fdbc1b71e3194ecf15a2ea37618ba0bfa22df3
amrudzr <amrudzr@gmail.com>
2025-11-23 23:16:04 +0700

    fix(owner-venue): resolve merge conflicts and clean up index, update, and destroy methods

--- app/Http/Controllers/API/V1/Owner/VenueController.php
+++ app/Http/Controllers/API/V1/Owner/VenueController.php
@@ -6,18 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\Owner\Venue\DestroyRequest;
 use App\Http\Requests\API\V1\Owner\Venue\StoreRequest;
 use App\Http\Requests\API\V1\Owner\Venue\UpdateRequest;
-<<<<<<< HEAD
-=======
-use App\Models\Owner;
->>>>>>> 5431e20e28c40d63fceb54a631c3a9d9e58160b4
 use App\Models\Venue;
 use App\Services\Owner\VenueService;
 use App\Traits\ApiResponseTrait;
 use Exception;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Auth;
-=======
->>>>>>> 5431e20e28c40d63fceb54a631c3a9d9e58160b4
 
 /**
  * @group Venue Management
@@ -35,7 +28,6 @@ class VenueController extends Controller
         $this->venueService = $venueService;
     }
 
-<<<<<<< HEAD
     public function index()
     {
         try {
@@ -62,14 +54,6 @@ class VenueController extends Controller
             $data = $venue->load(['owner:id,full_name,email', 'venuePhoto:id,venue_id,photo_url']);
 
             return $this->sendSuccessWithData($data, 'Berhasil menampilkan detail venue.', 200);
-=======
-    public function index(Owner $owner)
-    {
-        try {
-            $data = Venue::with('owner:id,full_name,email')->where('owner_id', $owner->id)->get();
-
-            return $this->sendSuccessWithData($data, 'Berhasil menampilkan.', 200);
->>>>>>> 5431e20e28c40d63fceb54a631c3a9d9e58160b4
         } catch (Exception $e) {
             return $this->sendInternalError($e);
         }
@@ -89,19 +73,12 @@ class VenueController extends Controller
     public function update(UpdateRequest $request, Venue $venue)
     {
         try {
-<<<<<<< HEAD
             $ownerId = Auth::guard('api_owner')->id();
 
             if ($venue->owner_id !== $ownerId) {
                 return $this->sendError('Tidak dapat mengakses venue yang bukan milik Anda.', [], 403);
             }
 
-=======
-            $owner = $request->user('api_owner');
-            if ($venue->owner_id !== $owner->id) {
-                return $this->sendError('Venue tidak bisa diubah.', [], 400);
-            }
->>>>>>> 5431e20e28c40d63fceb54a631c3a9d9e58160b4
             $update = $this->venueService->updateVenue($request->validated(), $venue);
 
             return $this->sendSuccessWithData($update, 'Venue berhasil diupdate.', 200);
@@ -113,19 +90,12 @@ class VenueController extends Controller
     public function destroy(DestroyRequest $request, Venue $venue)
     {
         try {
-<<<<<<< HEAD
             $ownerId = Auth::guard('api_owner')->id();
 
             if ($venue->owner_id !== $ownerId) {
                 return $this->sendError('Tidak dapat mengakses venue yang bukan milik Anda.', [], 403);
             }
 
-=======
-            $owner = $request->user('api_owner');
-            if ($venue->owner_id !== $owner->id) {
-                return $this->sendError('Venue tidak bisa dihapus.', [], 400);
-            }
->>>>>>> 5431e20e28c40d63fceb54a631c3a9d9e58160b4
             $delete = $this->venueService->deleteVenue($venue);
 
             return $this->sendSuccessWithData($delete, 'Venue berhasil dihapus.', 200);
