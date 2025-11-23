5431e20e28c40d63fceb54a631c3a9d9e58160b4
Amru Dzaky <amrudzr@gmail.com>
2025-11-23 17:54:01 +0700

    fix (auth): add role attribute for loginOwner method
    * fix (owner): add role attribute
    
    * fix (pint): make sure code using laravel pint

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Auth\LoginRequest;
 use App\Http\Requests\API\Auth\RegisterRequest;
+use App\Http\Resources\V1\Owner\OwnerResource;
 use App\Services\AuthService;
 use App\Traits\ApiResponseTrait;
 use Exception;
@@ -73,9 +74,11 @@ class AuthController extends Controller
         try {
             $result = $this->authService->attemptLogin('api_owner', $request->validated());
 
+            $user = $result['user'];
+
             return $this->sendSuccessWithData([
                 'token' => $result['token'],
-                'user' => $result['user'],
+                'user' => new OwnerResource($user),
             ], 'Login Owner berhasil.');
 
         } catch (ValidationException $e) {
