cb45ed9bea598f14ab970b64522762baa914ca22
amrudzr <amrudzr@gmail.com>
2025-12-12 14:16:16 +0700

    fix(model): refactor image output

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,16 +33,12 @@ class User extends Authenticatable
     protected function profilePhotoUrl()
     {
         return Attribute::make(
-            get: function ($value) {
-                if (! $value) {
-                    return null;
+            function ($value) {
+                if ($value) {
+                    return Storage::disk('public')->url($value);
                 }
 
-                if (str_contains($value, 'http')) {
-                    return $value;
-                }
-
-                return Storage::disk('public')->url($value);
+                return null;
             }
         );
     }
